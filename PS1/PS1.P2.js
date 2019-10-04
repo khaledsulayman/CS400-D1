565c49f634e8ddfb7c7fb6d21be97c0cda0b0527
Khaled Sleiman <Khaled@dhcp-wifi-8021x-168-122-201-83.bu.edu>
2019-10-04 13:26:41 -0400

    Committer: Khaled Sleiman <Khaled@dhcp-wifi-8021x-168-122-201-83.bu.edu>
     Changes to be committed:
    	modified:   PS1/PS1.P2.js

--- PS1/PS1.P2.js
+++ PS1/PS1.P2.js
@@ -1,28 +1,26 @@
 const evaluate = s => {
     const l = parseInt(s.charAt(0));
     const r = parseInt(s.charAt(2));
-    let expr;
 
     switch (s.charAt(1)) {
         case '+':
-            expr = l+r;
+            return () => l+r;
             break;
         case '-':
-            expr = l-r;
+            return () => l-r;
             break;
         case '*':
-            expr = l*r;
+            return () => l*r;
             break;
         case '/':
-            expr = l/r;
+            return () => l/r;
             break;
         case '%':
-            expr = l%r;
+            return () => l%r;
             break;
     }
-    return expr;
 }
 
 const expression = '8%3';
 let operator = evaluate(expression);
-console.log(`${expression} = ${operator}`);
+console.log(`${expression} = ${operator(expression)}`);
