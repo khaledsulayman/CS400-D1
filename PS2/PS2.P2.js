3977b2fac7b8c1e2083553227c476c0a8d0c52b5
Khaled Sleiman <Khaled@Walids-MacBook-Pro.local>
2019-10-20 22:07:19 -0400

    Committer: Khaled Sleiman <Khaled@Walids-MacBook-Pro.local>  Changes to be committed: 	modified:   PS2/PS2.P1.js 	modified:   PS2/PS2.P2.js

--- PS2/PS2.P2.js
+++ PS2/PS2.P2.js
@@ -1,19 +1,18 @@
 const forMan = function* (forMankind) {
+    /**
+     * Generator that emits the input string
+     * one word at a time.
+     */
     const oneGiantLeap = forMankind.split(' ');
-    for (let i = 0 ; ; i++)
+    for (let i = 0 ; i < oneGiantLeap.length; i++)
         yield oneGiantLeap[i];
+    // ( .done === true ) upon loop termination.
 }
 
-let oneSmallStep = forMan("All I know is something like a bird within her sang");
+let oneSmallStep = forMan("All I know is something like a bird within her sang"); // test case from handout
+let next = oneSmallStep.next();
 
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
-console.log(oneSmallStep.next().value);
+while (!next.done) {
+    console.log(next.value);
+    next = oneSmallStep.next();
+}
