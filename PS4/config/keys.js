a3baf80064c998d7125e8ddffe4a8e32fc4fbcb1
Khaled Sulayman <55605301+khaledsulayman@users.noreply.github.com>
2019-11-27 23:14:57 -0500

    Can't have these here lol

--- PS4/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    spotify: {
-        CLIENT_ID: '3ec313e8b0164df9b8d23d486332279a',
-        CLIENT_SECRET: 'd0aa6639bfb34d5084ab3c8fd0ad88c9',
-        REDIR_URI: 'http://localhost:3000/callback'
-    }
-};
